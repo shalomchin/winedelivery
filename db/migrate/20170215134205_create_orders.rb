4a6b63bbbe48bb91fe020fad676f5215435f44ee
Unknown <stanshalomchin@yahoo.com>
2017-02-15 21:48:34 +0800

    Create model for order and db migrate

---
+++ db/migrate/20170215134205_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.string :name
+      t.text :address
+      t.integer :qty1
+      t.integer :qty2
+      t.decimal :totalprice
+
+      t.timestamps
+    end
+  end
+end
