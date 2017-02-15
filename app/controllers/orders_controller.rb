1b5fec57296e1810332b888a29bfe50feca2a5ec
Unknown <stanshalomchin@yahoo.com>
2017-02-15 21:32:48 +0800

    Did forms for wine order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,13 @@
 class OrdersController < ApplicationController
 	def new
 	end
+
+	def create
+		render plain: params[:order].inspect
+	end
+
+	def show
+    @order = Order.find(params[:id])
+  end
+
 end
