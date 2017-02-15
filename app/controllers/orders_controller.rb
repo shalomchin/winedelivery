921dd1e31d5e4b91b9c07c75ae59928bbae1b8cf
Unknown <stanshalomchin@yahoo.com>
2017-02-15 22:15:49 +0800

    Convert .erm to .slim for new.html in orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,20 @@
 class OrdersController < ApplicationController
-	def new
-	end
+  def new
+  end
 
-	def create
-		render plain: params[:order].inspect
-	end
+  def create
+    @order = Order.new(order_param)
+    @order.save
+    redirect_to @order
+  end
 
-	def show
+  def show
     @order = Order.find(params[:id])
+  end
+ 
+ private
+  def order_param
+   params.require(:order).permit(:name,:address,:qty1,:qyty2,:totalprice)
   end
 
 end
