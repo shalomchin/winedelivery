69628cc567ac271f3881a45a52346b4dda3c4499
Unknown <stanshalomchin@yahoo.com>
2017-02-16 16:24:55 +0800

    Add validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+ validates :qty1, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+ validates :qty2, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
  validates :name, presence: true
  validates :address, presence: true
  validates :contact, presence: true
